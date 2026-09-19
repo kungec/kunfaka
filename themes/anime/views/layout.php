9a88c8cc703cd6256527197edd893366ffcf5eeb
baiyaodao <baiyaodao@gmail.com>
2026-09-20 06:52:20 +0800

    v2.22.1: 全站favicon(64x64 ico)+前台主题head引用

--- themes/anime/views/layout.php
+++ themes/anime/views/layout.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 <meta name="theme-color" content="#ff7eb3">
 <title><?= e(isset($pageTitle) ? $pageTitle : setting('site_name', '坤发卡')) ?></title>
+    <link rel="icon" href="<?= e(site_url('favicon.ico')) ?>" sizes="64x64">
 <link rel="stylesheet" href="<?= $_theme_url ?>assets/css/style.css?v=2.0.0">
 </head>
 <body>
--- themes/store/views/layout.php
+++ themes/store/views/layout.php
@@ -7,6 +7,7 @@
 <meta name="theme-color" content="#f5f5f7" media="(prefers-color-scheme: light)">
 <meta name="theme-color" content="#0a0a0b" media="(prefers-color-scheme: dark)">
 <title><?= e(isset($pageTitle) ? $pageTitle : setting('site_name', '坤发卡')) ?></title>
+    <link rel="icon" href="<?= e(site_url('favicon.ico')) ?>" sizes="64x64">
 <link rel="stylesheet" href="<?= $_theme_url ?>assets/css/style.css?v=1.0.0">
 <script>
 (function () {
